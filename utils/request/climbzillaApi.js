7af28b5368929df935eedd39759e7bd0c1dc2709
oleg <oleg.korobenko@gmail.com>
2017-08-19 15:03:58 +0300

    add data transformations for entities

--- utils/request/climbzillaApi.js
+++ utils/request/climbzillaApi.js
@@ -1,24 +1,92 @@
 const got = require('got');
 const Promise = require('bluebird');
+const _ = require('underscore');
 const configHolder = require('../../config/holder');
 
-// eslint-disable-next-line arrow-body-style
-const baseRequest = (path, options) => {
+const makeHall = (item) => {
+	return {
+		id: item.id,
+		name: item.name,
+		city: {name: item.city},
+		rotesCount: item.tops_count
+	};
+};
+
+const makeHalls = (items) => {
+	return items.map(makeHall);
+};
+
+const makeUser = (item) => {
+	return {
+		id: item.id,
+		fullName: item.full_name,
+		avatar: item.photo_200
+	};
+};
+
+const makePhoto = (item, {baseUrl}) => {
+	return {
+		id: item.id,
+		url: baseUrl + item.url,
+		description: item.description
+	};
+};
+
+const makeRoute = (item, {baseUrl}) => {
+	return {
+		id: item.id,
+		hall: {id: item.hall_id},
+		grade: item.grade,
+		title: item.title,
+		author: makeUser(item.author),
+		photos: item.photos.map((photo) => {
+			return makePhoto(photo, {baseUrl});
+		})
+	};
+};
+
+const makeRoutes = (items, options) => {
+	return items.map(makeRoute, options);
+};
+
+const baseRequest = (path, options = {}) => {
+	const gotOptions = _(options).omit('transform');
+	const transform = options.transform || _.identity;
+
 	return Promise.resolve()
-		.then(() => { return configHolder.get(); })
+		.then(() => {
+			return configHolder.get();
+		})
 		.then((config) => {
 			const {host, port} = config.services.climbzillaApi;
 			const baseUrl = `${host}:${port}`;
 
-			return got(baseUrl + path, Object.assign({json: true}, options));
+			const res = got(
+				baseUrl + path,
+				_(gotOptions).defaults({json: true})
+			);
+
+			return Promise.all([
+				config.services.climbzillaApi.baseUrl,
+				res
+			]);
 		})
-		.then((res) => { return res.body; });
+		.then(([baseUrl, res]) => {
+			return transform(res.body, {baseUrl});
+		});
 };
 
-exports.getHalls = () => { return baseRequest('/v03/hall'); };
+exports.getHalls = () => {
+	return baseRequest('/v03/hall', {transform: makeHalls});
+};
 
 exports.getRoutes = ({hallId}) => {
-	return baseRequest('/v03/top', {query: {hall_id: hallId}});
+	return baseRequest('/v03/top', {
+		query: {hall_id: hallId},
+		transform: makeRoutes
+	});
 };
 
-exports.getRoute = (routeId) => { return baseRequest(`/v02/top/${routeId}`); };
+exports.getRoute = (routeId) => {
+	return baseRequest(`/v02/top/${routeId}`, {transform: makeRoute});
+};
